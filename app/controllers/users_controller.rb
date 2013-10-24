9bc7c093d46c3da49e1a1c521bbb56e8e2c58ef7
Yungchih Chen <yungchih_chen@yahoo.com>
2013-10-24 14:46:51 -0400

    Added phone column to user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,6 +69,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :email, :avatar, :website)
+      params.require(:user).permit(:name, :email, :avatar, :website, :phone)
     end
 end
